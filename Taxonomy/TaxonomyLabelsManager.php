29898f5ecb700c0b96fdc0fc970f0eb6832bcddc
Guillaume <guillaume.turpin45@gmail.com>
2022-06-09 16:37:09 +0200

    :bug: Fix taxonomies label missing space

--- Taxonomy/TaxonomyLabelsManager.php
+++ Taxonomy/TaxonomyLabelsManager.php
@@ -25,15 +25,15 @@ final class TaxonomyLabelsManager {
 			'view_item'                  => __( 'View ' . $taxonomy->get_name(), '' ),
 			'update_item'                => __( 'Update ' . $taxonomy->get_name(), '' ),
 			'add_new_item'               => __( 'Add New ' . $taxonomy->get_name(), '' ),
-			'new_item_name'              => __( 'New ' . $taxonomy->get_name() . 'Name', '' ),
+			'new_item_name'              => __( 'New ' . $taxonomy->get_name() . ' Name', '' ),
 			'parent_item'                => __( 'Parent ' . $taxonomy->get_plural_name(), '' ),
 			'parent_item_colon'          => __( 'Parent ' . $taxonomy->get_plural_name(), '' ),
 			'search_items'               => __( 'Search ' . $taxonomy->get_plural_name(), '' ),
 			'popular_items'              => __( 'Popular ' . $taxonomy->get_plural_name(), '' ),
-			'separate_items_with_commas' => __( 'Seperate ' . $taxonomy->get_plural_name() . 'with commas', '' ),
+			'separate_items_with_commas' => __( 'Seperate ' . $taxonomy->get_plural_name() . ' with commas', '' ),
 			'add_or_remove_items'        => __( 'Add or remove ' . $taxonomy->get_plural_name(), '' ),
 			'choose_from_most_used'      => __( 'Choose from most used ' . $taxonomy->get_plural_name(), '' ),
-			'not_found'                  => __( 'No ' . $taxonomy->get_plural_name() . 'found', '' ),
+			'not_found'                  => __( 'No ' . $taxonomy->get_plural_name() . ' found', '' ),
 		];
 
 		// replace defaults with the options passed
